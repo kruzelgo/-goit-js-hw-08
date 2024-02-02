ae7113b7242d8620276ff20ba3a562aaa973b5c2
kruzelgo <kruzelgo@gmail.com>
2024-02-02 13:09:49 +0100

    task 1 i 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const imagesGallery = document.querySelector('.gallery');
+
+const gallery = galleryItems.map(item => {
+  const galleryList = document.createElement('li');
+  const link = document.createElement('a');
+  const image = document.createElement('img');
+
+  galleryList.classList.add('gallery__item');
+  link.classList.add('gallery__link');
+  image.classList.add('gallery__image');
+
+  link.href = item.original;
+  image.src = item.preview;
+  image.dataset.source = item.original;
+  image.alt = item.description;
+  image.title = item.description;
+
+  link.appendChild(image);
+  galleryList.appendChild(link);
+  return galleryList;
+});
+
+imagesGallery.append(...gallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const currentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', currentTime);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+if (storedTime) {
+  player
+    .setCurrentTime(parseFloat(storedTime))
+    .then(function (seconds) {
+      console.log('Seeked to:', seconds);
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.error(
+            'The time was less than 0 or greater than the video’s duration.'
+          );
+          break;
+
+        default:
+          console.error('Some other error occurred:', error);
+          break;
+      }
+    });
+}
